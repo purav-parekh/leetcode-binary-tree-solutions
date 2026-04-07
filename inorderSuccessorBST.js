a85d63705cc54839c22c15063732ca7ee830bd7b
Purav Parekh <purav98@gmail.com>
2026-04-07 11:25:06 -0400

    Optimize inorderSuccessor in BST
    Refactor inorderSuccessor for optimal performance.

--- inorderSuccessorBST.js
+++ inorderSuccessorBST.js
@@ -1,6 +1,7 @@
 // Given a BST, return the inorder successor of val.
 // Leetcode - 285 (Premium)
 
+// not optimal
 const inorderSuccessor = (root,val) => {
   let ans = null;
   const dfs = (node) => {
@@ -19,4 +20,16 @@ const inorderSuccessor = (root,val) => {
   }
   dfs(root)
   return ans
+}
+
+// optimal - goes either left or right = O(h)
+const inorderSuccessor = (root,val) => {
+  let successor = null;
+  while(root !== null) {
+    if(root.val > val) {
+      successor = root.val
+      root = root.left
+    } else root = root.right
+  }
+  return successor
 }
